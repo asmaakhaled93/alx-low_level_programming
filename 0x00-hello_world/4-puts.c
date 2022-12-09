26703a0c022ef89ce2a94c7d46ae51b24de3e5c2
asmaakhaled93 <asmaakhaledfarrag>
2022-12-08 16:00:50 -0800

    adding up

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-
 /**
+ * main - Entry
  *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: Always 0
  *
- *   * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
+ */
 
 int main(void)
-
 {
-
-
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
