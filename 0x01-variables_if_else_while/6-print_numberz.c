15926572d34673621a5b695ef8fa694e1ea49c9b
asmaakhaled93 <asmaakhaledfarrag>
2022-12-09 16:30:36 -0800

    going forward

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+
 /**
  *  main - prints out all numbers under 10
+ *  without using any char variables
  *  Return: 0
  */
+
 int main(void)
 {
-	int i;
+	int i = 48;
 
-	for (i = 0; i < 10; i++)
-		printf("%i", i);
-	printf("\n");
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
