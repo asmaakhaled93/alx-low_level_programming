e8ee35ba85f08da6c41fbb2a929ff96badae57f9
asmaakhaled93 <asmaakhaledfarrag>
2022-12-15 17:07:18 -0800

    going forward

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,9 @@
  * Return: 1 if true, 0 if false
  */
 int _isupper(int c)
+
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-	return (0);
-	}
+
+		return (c >= 'A' && c <= 'Z');
+
 }
