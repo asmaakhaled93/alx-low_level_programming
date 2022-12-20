4141f24cd5e8bf35c8e591f600e3a7a65ada965e
asmaakhaled93 <asmaakhaledfarrag>
2022-12-19 18:32:19 -0800

    going forward

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,17 +7,19 @@
 *
 * Return: void
 */
-
 void print_rev(char *s)
 {
-	int c = 0;
+	int c = 0
+		;
 	while (s[c] != '\0')
 	{
 		c++;
 	}
+
 	for (c -= 1; c >= 0; c--)
 	{
 		_putchar(s[c]);
 	}
+	
 	_putchar('\n');
 }
