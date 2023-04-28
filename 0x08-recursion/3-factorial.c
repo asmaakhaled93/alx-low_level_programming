ccf286d0297f3736e02bbeb2747d886af830c683
Asmaa Khaled <asmaakhaledfarrag@gmail.com>
2023-04-27 19:21:33 -0700

    fix

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,13 +1,20 @@
 #include "main.h"
 
+/**
+ *factorial - factorial of a given number
+ *@n: pointer block of memory to fill
+ *Return: factorial
+ */
+
 int factorial(int n)
 {
-if (n == 0)
-	return (1);
+if (n == 0) /*Base condition*/
 
-else if (n < 0) 
+return (1);
+
+else if (n < 0) /*Base condition*/
 return (-1);
 else
-return (n * factorial(n - 1);
+return (n * factorial(n - 1)); /*Recursive call*/
 
 }
